fe3c6474933ec2b54418470ad537d0f9d7888341
Serhii Kopach <Serega@MacBook-Air.local>
2015-09-17 20:53:59 +0300

    v2.0

---
+++ GitTest2/GitTest2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GitTest2
+//
+//  Created by Serhii Kopach on 17.09.15.
+//  Copyright (c) 2015 Serhii Kopach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World 2!");
+    }
+    return 0;
+}
